a03da1d4aa0de3b26197ac6ad6970ee7c63aca27
Naitokeisuke <sakura.game.0527@gmail.com>
2015-10-14 17:45:20 +0900

    弾の発射と前方に動く

---
+++ OculusGame/Assets/Player3D/Script/Player3DBulletShot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player3DBulletShot : MonoBehaviour {
+
+	//弾を設定
+	[SerializeField]
+	private GameObject player3DBullet;
+
+	//プレイヤーを設定
+	[SerializeField]
+	private Transform player;
+
+	// Update is called once per frame
+	void Update () {
+		ShotBullet ();
+	}
+
+	//弾を発射（弾を呼び出す）
+	void ShotBullet(){
+		Instantiate(player3DBullet,player.position,player.rotation);
+	}
+}
