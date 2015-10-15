f22cd2db04ffa0508d394127ebf964afa0554f4c
kaseda <hieharu3216@gmail.com>
2015-10-15 13:09:51 +0900

    2Dスコア表示 徐々に加算されるようになってます。 スコアはDataManagerで管理してます

---
+++ OculusGame/Assets/Score2D/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+    public int score { get; private set; }
+    int scoreYard;
+    GameObject scoreView2D;
+
+	// Use this for initialization
+	void Awake () {
+        score = 0;
+        scoreYard = 0;
+        scoreView2D = GameObject.Find("ScorePoint");
+	}
+
+    public void plusScore(int plus){
+
+        scoreYard += plus;
+        StartCoroutine(Coroutine());
+        
+    }
+
+    IEnumerator Coroutine()
+    {
+        while (scoreYard > 0)
+        {
+            score += 100;
+            scoreYard -= 100;
+            scoreView2D.GetComponent<ScoreView2D>().DrawScore();
+            yield return null;
+        }
+    }
+}
---
+++ OculusGame/Assets/Score2D/test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    GameObject scoreManager;
+
+	// Use this for initialization
+	void Awake () {
+        scoreManager = GameObject.Find("DataManager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            scoreManager.GetComponent<ScoreManager>().plusScore(20000);
+        }
+	}
+}
