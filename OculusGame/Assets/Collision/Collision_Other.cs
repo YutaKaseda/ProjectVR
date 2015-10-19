c8d50e1e6a332f3e39c306106219d080fd9790dc
NaitoKeisuke <sakura.game.0527@gmail.com>
2015-10-19 11:39:07 +0900

    弾の処理現段階完成

---
+++ OculusGame/Assets/Collision/Collision_Other.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision_Other : MonoBehaviour {
+    
+    void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            //弾に当たったら
+            case "bullet":
+
+                //HPを減らすとか
+
+                break;
+                
+            default:
+                break;
+        }
+
+    }
+
+}
---
+++ OculusGame/Assets/Collision/Collision_bullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision_bullet : MonoBehaviour {
+
+    //当たらなくても5秒後には消えるように
+    void Awake()
+    {
+        Destroy(gameObject,5);
+    }
+
+    //何かに当たったら消える
+	void OnTriggerEnter(Collider other){
+		Destroy (gameObject);
+	}
+}
