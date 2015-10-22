45da7899a730199f06fb7388144d0b53b9c77cb7
YutaKaseda <yutakaseda3216@gmail.com>
2015-10-22 15:49:33 +0900

    神がおりたのでリソース管理のベースが完成しました

--- OculusGame/Assets/Scripts/ResourcesManager.cs
+++ OculusGame/Assets/Scripts/ResourcesManager.cs
@@ -1,25 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using Serialize;
 
-public class ResourcesManager : SingletonMonobehaviour<ResourcesManagerS> {
+public class ResourcesManager : SingletonMonobehaviour<ResourcesManager> {
 
+	[SerializeField]
+	Dictionary<string,GameObject> resourcesDictionary = new Dictionary<string, GameObject>();
 
-
-	class ResourcesInfo{
-		public string resourcesName;
-		public string name;
-		public GameObject obj;
-
-		ResourcesInfo(string resourcesName,string name){
-			this.resourcesName = resourcesName;
-			this.name = name;
-		}
-	}
+	[SerializeField]
+	GameObject prefabResources;
+	[SerializeField]
+	GameObject textureResources;
+	[SerializeField]
+	GameObject materialResources;
 
 	void Awake(){
+		resourcesDictionary.Add("Prefab",prefabResources);
+		resourcesDictionary.Add("Texture",textureResources);
+		resourcesDictionary.Add("Material",materialResources);
 
-		if()
+		var obj = Instantiate(prefabResources);
+		obj.transform.parent = transform;
 
+		obj = Instantiate(textureResources);
+		obj.transform.parent = transform;
+
+		obj = Instantiate(materialResources);
+		obj.transform.parent = transform;
 	}
 
+	public GameObject GetResource(string resourceStuckName,string resourceName){
+
+		switch(resourceStuckName){
+		case "Prefab":
+
+			return resourcesDictionary[resourceStuckName].GetComponent<PrefabResources>().IResourcesList(resourceName);
+
+			break;
+		default:
+			break;
+		}
+
+		return null;
+	}
 }
