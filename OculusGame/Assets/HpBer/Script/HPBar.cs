2ac8a3b109e95fec97b92ffc1a9ac32542a6e00e
KentaShimura <team.shimuken@gmail.com>
2015-10-28 15:08:39 +0900

    HPバー変更後

--- OculusGame/Assets/HpBer/Script/HPBar.cs
+++ OculusGame/Assets/HpBer/Script/HPBar.cs
@@ -4,57 +4,23 @@ using UnityEngine.UI;
 
 public class HPBar : MonoBehaviour {
 
-
+[SerializeField]
 Slider Slider;
-
-[^
-void Start () {
-}
-
+	
 	void Awake(){
-		// スライダーを取得する
-		Slider = GameObject.Find("Slider").GetComponent<Slider>();
-
-
-
-
-
-		//他のscriptから参照
-		//Player = GetComponent<Player>();
-		//Player.Hp=Player.Hp;
-
-		//Player.DoSomething();
-
-	
-
-
+		Slider.maxValue= Player_Hp;
 	}
-
-	public float Player_Hp=60;
-
-
+	public float Player_Hp;
 	void Update () {
-
-
-			Player_Hp -=1f;
-	
 		Player_Hp = HpBar(Player_Hp);
-
-
-
-
-	}
-
+			}
 	float HpBar(float Hp){
-		if(Hp < Slider.minValue) {
+		if(Hp <Slider.minValue) {
 			// 0になったら最大に戻す
 			Hp = Slider.maxValue;
 		}
-		
 		// HPゲージに値を設定
 		Slider.value = Hp;
-
-		return Hp;
+			return Hp;
 	}
-
 }
