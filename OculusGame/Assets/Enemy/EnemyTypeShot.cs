a1ec7b193fdc2a150ddb9ab993ca88681efcef67
umemuradaiki <i_like_uverworld__daiki__716@docomo.ne.jp>
2015-10-29 10:28:29 +0900

    敵の制御、修正

--- OculusGame/Assets/Enemy/EnemyTypeShot.cs
+++ OculusGame/Assets/Enemy/EnemyTypeShot.cs
@@ -5,15 +5,20 @@ public class EnemyTypeShot : MonoBehaviour {
 	GameObject enemyBulletPrefab;//敵のたま
 	Vector3 createPos;//生成された位置を記録
 	Vector3 movePos;//移動先の位置
-	float stopTime;//射撃停止秒数
-	int enemyBulletRapid;//敵のたまの連射速度
+	int stopTime;//射撃停止秒数
+	float enemyBulletRapid;//敵のたまの連射間隔
+	Transform player2DPosition;//
+	Vector3 targetPlayerVec;
+	GameObject targetPlayerGO;
+	EnemyBullet target;
 
 	void Awake () {
 		enemyBulletPrefab = Resources.Load ("Prefab/EnemyBullet") as GameObject;
 		createPos = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
 		movePos = new Vector3 (Random.Range (-5, 6), Random.Range (1, 10), Random.Range (8, 15));
-		stopTime = 5.0f;
-		enemyBulletRapid = 0;
+		stopTime = 5;
+		enemyBulletRapid = 1.0f;
+		player2DPosition = GameObject.FindWithTag("Player").transform;
 		StartCoroutine("EnemyTypeShotMove");
 	}
 
@@ -22,22 +27,26 @@ public class EnemyTypeShot : MonoBehaviour {
 			transform.position = Vector3.MoveTowards (transform.position, movePos, 0.2f);
 			yield return null;
 		}
+		StartCoroutine("EnemyTypeShotFire");
+	}
+
+	IEnumerator EnemyTypeShotFire(){
+		while(stopTime > 0){
+			stopTime -= 1;
+
+			targetPlayerVec = player2DPosition.transform.position;
+			targetPlayerGO = Instantiate(enemyBulletPrefab, transform.position, transform.rotation) as GameObject;
+			target = targetPlayerGO.GetComponent<EnemyBullet>();
+			target.EnemyShotMove(targetPlayerVec);
+			//この一連の流れで、EnemyBullet側にPlayerの位置を与えてる
+			yield return new WaitForSeconds(enemyBulletRapid);
+		}
 		StartCoroutine("EnemyTypeShotReturn");
 	}
 
 	IEnumerator EnemyTypeShotReturn(){
 		while(transform.position != createPos){
-			stopTime -= Time.deltaTime;
-			enemyBulletRapid += 1;
-
-			if(enemyBulletRapid >= 50 && stopTime >= 0.0f){
-				Instantiate(enemyBulletPrefab, transform.position, transform.rotation);
-				enemyBulletRapid = 0;
-			}
-
-			if(stopTime <= 0.0f){
-				transform.position = Vector3.MoveTowards (transform.position, createPos, 0.2f);
-			}
+			transform.position = Vector3.MoveTowards (transform.position, createPos, 0.2f);
 			yield return null;
 		}
 		Delete ();//消去
