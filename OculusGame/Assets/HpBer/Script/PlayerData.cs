4a7356e6ce33eda71235c19e36e9d025ccf5c187
KentaShimura <team.shimuken@gmail.com>
2015-10-29 17:03:33 +0900

    HPバー訂正3回目

---
+++ OculusGame/Assets/HpBer/Script/PlayerData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerData : MonoBehaviour {
+
+	[SerializeField]
+	HPBar HPBar;
+
+	public float HP{ get; private set; }
+	
+	void Awake(){
+			HP = 100;
+		   
+	}
+	void Update(){
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			HP-=10;
+			HPBar.Draw();
+			}
+	}
+}
