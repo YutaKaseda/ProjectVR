fcaff78f18f852318935f853cb178d269dc70fe0
KentaShimura <team.shimuken@gmail.com>
2015-11-02 10:30:08 +0900

    3D表示用スクリプト(Score_2Dを参考、引用)

---
+++ OculusGame/Assets/Score3D/score_3d.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;	//UI用
+
+public class score_3d : MonoBehaviour 
+{
+	public int score { get; private set; }
+	int scoreYard;
+	GameObject scoreView2D;
+	
+	// Use this for initialization
+	void Awake () {
+		score = 0;
+		scoreYard = 0;
+		scoreView2D = GameObject.Find("ScorePoint");
+	}
+	
+	public void plusScore(int plus){
+		
+		scoreYard += plus;
+		StartCoroutine(Coroutine());
+		
+	}
+	
+	IEnumerator Coroutine()
+	{
+		while (scoreYard > 0)
+		{
+			score += 100;
+			scoreYard -= 100;
+			scoreView2D.GetComponent<ScoreView2D>().DrawScore();
+			yield return null;
+		}
+	}
+}
