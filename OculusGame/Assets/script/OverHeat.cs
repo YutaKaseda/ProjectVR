e21bdee91f8c949bbd80f8df0a2753408d708f72
KentaShimura <team.shimuken@gmail.com>
2015-11-26 09:43:18 +0900

    2Dの武器アイコンなどのUI

---
+++ OculusGame/Assets/script/OverHeat.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OverHeat: MonoBehaviour {
+
+	[SerializeField]
+	Slider Bom;
+
+	[SerializeField]
+	Slider Cannon;
+
+	[SerializeField]
+	GameObject Player;
+
+
+
+
+
+	void Awake(){
+		Cannon.minValue = Player.GetComponent<PlayerData>().CannonGage;
+		Bom.minValue = Player.GetComponent<PlayerData>().BomGage;
+	 
+	}
+	public void Draw(){
+		Bom.value = Player.GetComponent<PlayerData>().BomGage;
+		Cannon.value = Player.GetComponent<PlayerData>().CannonGage;
+	
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+public class OverHeat: MonoBehaviour {
+	
+	[SerializeField]
+	Slider slider;
+	
+	[SerializeField]
+	GameObject Player;
+	
+	void Awake(){
+		slider.minValue = Player.GetComponent<PlayerData>().Gage;
+	}
+	public void Draw(){
+		slider.value = Player.GetComponent<PlayerData>().Gage;
+	}
+}*/
---
+++ OculusGame/Assets/script/PlayerData.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerData : MonoBehaviour {
+
+	[SerializeField]
+	OverHeat overheat;
+
+
+
+	public float CannonGage{ get; private set; }
+	public float BomGage{ get; private set; }
+
+
+	bool[] Gageflg = new bool[2] {
+	 true,true};
+
+
+
+	void Awake(){
+		CannonGage = 0;
+		BomGage = 0;
+	
+	}
+
+	void Update(){
+		if (Input.GetKey (KeyCode.Z) && Gageflg [0] == true) {
+			CannonGage += 0.1f;
+			if (CannonGage >= 100) {
+				Gageflg [0] = false;
+			}
+		} else {
+			CannonGage -= 0.5f;
+			if (CannonGage <= 0) {
+				CannonGage = 0;
+				Gageflg [0] = true;
+			}
+		}
+
+		if (Input.GetKey (KeyCode.X) && Gageflg[1] == true) {
+			BomGage = 100;
+			Gageflg[1] = false;
+		} else {
+			BomGage -= 0.3f;
+			if (BomGage <= 0) {
+				BomGage = 0;
+				Gageflg[1]= true;
+			}
+		}
+		overheat.Draw ();
+
+		}
+}
+			
+			
+		
