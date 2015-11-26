4fcbfb6f4d5c883e1842207599fa875c099efa97
NaitoKeisuke <sakura.game.0527@gmail.com>
2015-11-26 10:37:36 +0900

    Beacon 置く感覚の調整 置いてる時のゲージ シゲのビーコンHP、ビーコン爆破

--- OculusGame/Assets/Scripts/Player2D/Player2D.cs
+++ OculusGame/Assets/Scripts/Player2D/Player2D.cs
@@ -4,21 +4,35 @@ using System.Collections;
 public class Player2D : MonoBehaviour {
 	
     PlayerData2D playerData;
+    Beacon playerBeacon;
+    BeaconUI playerBeaconUI;
 
 	// Use this for initialization
 	void Awake () {
         playerData = GetComponent<PlayerData2D>();
+        playerBeacon = GetComponent<Beacon>();
+        playerBeaconUI = GetComponent<BeaconUI>();
         playerData.speed = 8.0f;
         playerData.bulletPrefab = Resources.Load("Prefab/Bullet2D") as GameObject;
 	}
     public void Move()
     {
-        playerData.vectorZ = Input.GetAxisRaw("HorizontalP2");
-        playerData.vectorY = Input.GetAxisRaw("VerticalP2");
+        if(playerBeacon.moveFlg == false){
+            playerData.vectorZ = Input.GetAxisRaw("HorizontalP2");
+            playerData.vectorY = Input.GetAxisRaw("VerticalP2");
+        }
+        if (playerBeacon.moveFlg == true)
+        {
+            playerData.vectorY = 0;
+            playerData.vectorZ = 0;
+        }
+ 
         playerData.movePlayer = new Vector3(0, playerData.vectorY * playerData.speed, playerData.vectorZ * playerData.speed);
         GetComponent<Rigidbody>().velocity = playerData.movePlayer;
         
         BulletShot();
+        playerBeacon.BeaconPut();
+        playerBeaconUI.BeaconPutUI();
 	}
 
     void BulletShot()
