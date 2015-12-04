32da689779c6804b059fd6aa7f497bc3edf93562
SuzukiRyohei <ryo0718s@yahoo.co.jp>
2015-12-04 10:30:46 +0900

    wave実装

--- OculusGame/Assets/Scripts/Enemy/EnemyFactory.cs
+++ OculusGame/Assets/Scripts/Enemy/EnemyFactory.cs
@@ -2,27 +2,23 @@
 using System.Collections;
 
 public class EnemyFactory : MonoBehaviour {
-	GameObject enemyTypeAttackPrefab;//突撃型
-	GameObject enemyTypeShotPrefab;//射撃型
 	Vector3 enemyTypeShotPos;//射撃型の生成位置
 
-	void Awake () {
-		enemyTypeAttackPrefab = Resources.Load ("Prefab/EnemyTypeAttack") as GameObject;
-		enemyTypeShotPrefab = Resources.Load ("Prefab/EnemyTypeShot") as GameObject;
-	}
-	
-	void Update () {
-		Create ();
-	}
-
-	void Create(){
-
-		if (Input.GetKeyDown(KeyCode.Z)) {
-			Instantiate(enemyTypeAttackPrefab, transform.position, transform.rotation);
-		}
-		if (Input.GetKeyDown(KeyCode.X)) {
+	public void Create(string ss){
+		switch (ss) {
+		case "enemy01":	//特攻型
+			Instantiate (ResourcesManager.Instance.GetResourceScene("EnemyTypeAttack"), transform.position, transform.rotation);
+			break;
+		case "enemy02":	//射撃型
 			enemyTypeShotPos = new Vector3 (Random.Range (-5, 6), Random.Range (0, 10), 20);
-			Instantiate(enemyTypeShotPrefab, enemyTypeShotPos, transform.rotation);
+			Instantiate (ResourcesManager.Instance.GetResourceScene("EnemyTypeShot"), enemyTypeShotPos, transform.rotation);
+			break;
+		case "medium01": //中ボス
+			Instantiate (ResourcesManager.Instance.GetResourceScene("EnemyTypeMedium"), transform.position, transform.rotation);
+			break;
+		case "boss01": //大ボス
+			Instantiate (ResourcesManager.Instance.GetResourceScene("EnemyTypeBoss"), transform.position, transform.rotation);
+			break;
 		}
 	}
 }
--- OculusGame/Assets/Scripts/Enemy/EnemyTypeShot.cs
+++ OculusGame/Assets/Scripts/Enemy/EnemyTypeShot.cs
@@ -2,23 +2,23 @@
 using System.Collections;
 
 public class EnemyTypeShot : MonoBehaviour {
-	GameObject enemyBulletPrefab;//敵のたま
 	Vector3 createPos;//生成された位置を記録
 	Vector3 movePos;//移動先の位置
 	int stopTime;//射撃停止秒数
 	float enemyBulletRapid;//敵のたまの連射間隔
-	Transform player2DPosition;//
+	Transform player2DPosition;
+	Transform player3DPosition;
 	Vector3 targetPlayerVec;
 	GameObject targetPlayerObject;
 
 
 	void Awake () {
-		enemyBulletPrefab = Resources.Load ("Prefab/EnemyBullet") as GameObject;
 		createPos = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
 		movePos = new Vector3 (Random.Range (-5, 6), Random.Range (1, 10), Random.Range (8, 15));
 		stopTime = 5;
 		enemyBulletRapid = 1.0f;
-		player2DPosition = GameObject.FindWithTag("Player").transform;
+		player2DPosition = GameObject.FindWithTag("Player2D").transform;
+		player3DPosition = GameObject.FindWithTag("Player3D").transform;
 		StartCoroutine("EnemyTypeShotMove");
 	}
 
@@ -33,9 +33,8 @@ public class EnemyTypeShot : MonoBehaviour {
 	IEnumerator EnemyTypeShotFire(){
 		while(stopTime > 0){
 			stopTime -= 1;
-
-			targetPlayerVec = player2DPosition.transform.position;
-			targetPlayerObject = Instantiate(enemyBulletPrefab, transform.position, transform.rotation) as GameObject;
+			targetPlayerVec = player2DPosition.transform.position; // 後でランダム処理にする
+			targetPlayerObject = Instantiate(ResourcesManager.Instance.GetResourceScene("EnemyBullet"), transform.position, transform.rotation) as GameObject;
 			targetPlayerObject.GetComponent<EnemyBullet>().EnemyShotMove(targetPlayerVec);
 			//この一連の流れで、EnemyBullet側にPlayerの位置を与えてる
 			yield return new WaitForSeconds(enemyBulletRapid);
