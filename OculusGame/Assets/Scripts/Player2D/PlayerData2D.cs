b8e23fdc00186df1a517a7d48a8ffaf7bde17d7a
Kenta Shimura <team.shimuken@gmail.com>
2015-12-09 15:13:51 +0900

    一昨日あげたスクリプトの修正版です。

--- OculusGame/Assets/Scripts/Player2D/PlayerData2D.cs
+++ OculusGame/Assets/Scripts/Player2D/PlayerData2D.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class PlayerData2D : PlayerData {
 
 	public float resurrectionTime{ get; set; }		//復活時のクールタイム
 	public float resurrectionPenalty{ get; set;}
+
+	public float cannonCoolTIme{ get;set; }
+	public float bomCoolTime{ get;set; }
+	public int beacon=6;
+	public Text text;
 }
