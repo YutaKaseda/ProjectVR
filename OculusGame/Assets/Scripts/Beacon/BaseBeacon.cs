7a597cdfd8b889203c0febcb8bcf068f8449eb4f
Keigo Nakamura <k5nakamura0120@yahoo.co.jp>
2015-12-14 09:34:39 +0900

    no message

--- OculusGame/Assets/Scripts/Beacon/BaseBeacon.cs
+++ OculusGame/Assets/Scripts/Beacon/BaseBeacon.cs
@@ -4,11 +4,10 @@ using System.Collections;
 public class BaseBeacon : MonoBehaviour {
 	enum beaconState{ENEMYCHARACTER, ENEMYBULLET}; //ダメージを受ける条件：敵キャラ、弾に当たる、自爆
 	[SerializeField]
-	int baseBeaconLife;
+	int baseBeaconLife;		//ベースビーコンのHP
 	GameObject explosion; //爆発時のエフェクト
 	
 	void Awake () {	
-		baseBeaconLife = 30;	//ベースビーコンのHP
 		//explosion = Resources.Load ("Prefab/Explosion") as GameObject; 
 		//↓に変更
 		ResourcesManager.Instance.ResourcesLoadScene ("Play");
