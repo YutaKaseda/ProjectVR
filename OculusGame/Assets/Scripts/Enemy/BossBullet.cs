070caaf359214cf7aa6160ea42aa4e6bf3d897c5
unknown <nwuser@YDA_GAME_M1.da.iwasaki.ac.jp>
2016-01-26 13:00:25 +0900

    Boss

---
+++ OculusGame/Assets/Scripts/Enemy/BossBullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossBullet : MonoBehaviour {
+	Vector3 enemyDirection;
+	float enemyBulletSpeed;//弾の速さ
+	float randomX,randomY,randomZ;
+	// Use this for initialization
+	void Awake () {
+		enemyBulletSpeed = 2400.0f;//弾の速さ
+		Destroy(gameObject,2f);
+	}
+
+	public void RandomBullet(){
+		randomX = Random.Range (-15,15);
+		randomY = Random.Range (-15,15);
+		randomZ = Random.Range (-15,15);
+		transform.Rotate (randomX, randomY, randomZ);
+		enemyDirection = transform.forward * enemyBulletSpeed;
+		GetComponent<Rigidbody> ().AddForce(enemyDirection);
+	}
+
+	public void TargetBullet(Vector3 target){
+		transform.LookAt (target);
+		enemyDirection = transform.forward * enemyBulletSpeed;
+		GetComponent<Rigidbody> ().AddForce(enemyDirection);
+	}
+
+}
