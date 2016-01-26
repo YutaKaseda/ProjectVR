8264f9438152915419d5b639206b3bd1860a65d0
Keigo Nakamura <k5nakamura0120@yahoo.co.jp>
2016-01-26 15:57:51 +0900

    waveupdate

--- OculusGame/Assets/Scripts/wave/WaveUpdate.cs
+++ OculusGame/Assets/Scripts/wave/WaveUpdate.cs
@@ -8,10 +8,13 @@ public class WaveUpdate : MonoBehaviour {
 	public static readonly string ENEMY_DATAEND = "END";
 	public static readonly string DATANULL = "NULL";
 	public static readonly string WAVEDATE_END = "END,-9999,-9999,-9999,-9999";
+	public static readonly string WAVE_BOSS = "boss";
 
 	[SerializeField]
 	List<TextAsset> waveTextList = new List<TextAsset>();
 	EnemyFactory enemyFactory;
+	[SerializeField]
+	ResultScore resultScore;
 
 	class EnemyDate{
 		public string enemyName{ get; set; }	// 呼び出す敵の名前	
@@ -25,17 +28,23 @@ public class WaveUpdate : MonoBehaviour {
 	EnemyDate[] waveData;
 
 	[SerializeField]
-	int arrayBox;
-	int waveState;
-	int arrayPosition;
-	float startTime;
-	float rapTime;
-
+	int arrayBox;			// 配列の初期個数100くらいに設定してください
+	int waveState;			// Waveの進行数
+	int arrayPosition;		// 配列の今の場所,敵生成時に使用
+	float startTime;		// タイマーの開始時間
+	float timeRap;			// 特定のタイミングの時間を記憶するときに使用
+	[SerializeField]
+	float stayTime;			// Waveが終わってどのくらい待つか
 	string[] waveInfoArray;
 	[SerializeField]
 	List<string> waveInfo = new List<string>();
+	bool socreFlg = false;
+	public bool bossFlg = false;
+	[SerializeField]
+	Beacon beacon;
 
 	void Awake () {
+		waveTextList.Add(null);
 		enemyFactory = GameObject.Find ("EnemyFactory").GetComponent<EnemyFactory> ();
 		waveState = 0;
 
@@ -49,34 +58,45 @@ public class WaveUpdate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (beacon.baseBeacon == true)
 			Wave ();
+		else
+			timeInit ();
 	}
 
-	void Wave(){
+	public void Wave(){
 
-		if (waveData[arrayPosition].enemyName != DATANULL &&
-		    waveData [arrayPosition].enemyName != ENEMY_DATAEND) {
+		if (waveData [arrayPosition].enemyName != DATANULL &&
+			waveData [arrayPosition].enemyName != ENEMY_DATAEND &&
+		    bossFlg == false) {
 
 			if (waveData [arrayPosition].delayTime <= (Time.realtimeSinceStartup - startTime)) {
-			
-				enemyFactory.Create(waveData[arrayPosition].enemyName ,
-				                    new Vector3(waveData[arrayPosition].pointX ,
-				            					waveData[arrayPosition].pointY ,
-				            					waveData[arrayPosition].pointZ)
-				                    );
+				enemyFactory.Create (waveData [arrayPosition].enemyName,
+				                    new Vector3 (waveData [arrayPosition].pointX,
+				            					waveData [arrayPosition].pointY,
+				            					waveData [arrayPosition].pointZ)
+				);
+				if(waveData[arrayPosition].enemyName.Contains(WAVE_BOSS)){
+					bossFlg = true;
+				}
 				arrayPosition++;
+			} else {
+				timeRap = Time.realtimeSinceStartup - startTime;
 			}
 
-		} else {
+		} else if(bossFlg == true) {
 
-			Debug.Log ("waveが終わったよ");
-			waveState++;
-			// 配列の初期化
-			DataInit();
-			// 次のテキストの読込
-			LoadText();
-			// タイマーのリセット
-			timeInit();
+		}else {
+
+			if(socreFlg == false && (Time.realtimeSinceStartup - startTime) >= (timeRap + (stayTime))){
+				resultScore.InitScoreText();
+				socreFlg = true;
+			}
+			if(socreFlg == true && resultScore.resultViweFlg == false){
+				socreFlg = false;
+				NextWave();
+			}
 		}
 	}
 
@@ -90,6 +110,7 @@ public class WaveUpdate : MonoBehaviour {
 		if (waveTextList [waveState] == null) {
 			// ゲーム終了
 			Debug.LogError ("TextNull");
+			this.gameObject.SetActive(false);
 			return;
 		}
 		waveInfoArray = waveTextList [waveState].text.Split ('\n');
@@ -120,18 +141,29 @@ public class WaveUpdate : MonoBehaviour {
 		waveData = new EnemyDate[arrayBox];
 
 		// クラスの初期化
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < arrayBox; i++) {
 			waveData[i] = new EnemyDate(DATANULL,-9999);
 		}
 
 		// 配列の位置
 		arrayPosition = 0;
+		waveInfo.Clear ();
 	}
 	
 	void timeInit(){
 		startTime = Time.realtimeSinceStartup;
 	}
 
+	// 次のwaveへ
+	void NextWave(){
+		waveState++;
+		// 配列の初期化
+		DataInit();
+		// 次のテキストの読込
+		LoadText();
+		// タイマーのリセット
+		timeInit();
+	}
 	IEnumerator ArrayWhile(){
 		int arrayInt = 0;
 		while(waveInfo[arrayInt] != WAVEDATE_END) {
@@ -148,5 +180,4 @@ public class WaveUpdate : MonoBehaviour {
 
 		yield return null;
 	}
-
 }
