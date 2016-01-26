64af341ea014a4581d7305ab1a0ada9de1a9c8c3
Keigo Nakamura <k5nakamura0120@yahoo.co.jp>
2016-01-26 15:34:49 +0900

    石田 + 瀧本 リザルト + ベースビーコン

---
+++ OculusGame/Assets/baseBeacon/script/Installation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+//２Dのベースビーコンの設置　２Dの移動の所に入れてくさい
+public class Installation : MonoBehaviour {
+
+	[SerializeField]
+	Player3D P3d;
+
+
+	public GameObject prefab;
+
+
+
+	/*	if (ボタン処理) {
+			BaseBeacon();
+		}
+*/
+	
+
+	
+	//BaseBeacon設置関数
+	void BaseBeacon(){
+		P3d.basebeacon = transform.position;
+		Instantiate(prefab,transform.position,transform.rotation);
+	
+	}
+
+
+	
+	
+}
