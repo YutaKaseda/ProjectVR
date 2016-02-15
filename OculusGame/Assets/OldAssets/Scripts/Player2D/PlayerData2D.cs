d69c0dd1dcd74376e9bf5ee477e904ceacbf1274
SuzukiRyohei <ryo0718s@yahoo.co.jp>
2016-02-15 17:34:42 +0900

    プレイヤー2D動作コミット

--- OculusGame/Assets/OldAssets/Scripts/Player2D/PlayerData2D.cs
+++ OculusGame/Assets/OldAssets/Scripts/Player2D/PlayerData2D.cs
@@ -7,9 +7,14 @@ public class PlayerData2D : PlayerData {
 
 	public float resurrectionTime{ get; set; }		//復活時のクールタイム
 	public float resurrectionPenalty{ get; set;}
-
 	public float cannonCoolTIme{ get;set; }
 	public float bomCoolTime{ get;set; }
 	public int beacon=6;
 	public Text text;
+	public Vector3 moveValue;
+	public float degree{ get; set;}
+	[SerializeField]
+	public float radius;
+	public float pi{ get; set;}
+	public Vector3 position;
 }
