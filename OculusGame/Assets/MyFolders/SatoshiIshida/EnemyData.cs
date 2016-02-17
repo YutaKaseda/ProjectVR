03cb89ce9d7eb1f446dcceace1d4e9fce3de8a57
unknown <nwuser@YDA_GAME_M1.da.iwasaki.ac.jp>
2016-02-17 18:21:37 +0900

    敵にダメージを与えた時のエフェクト発生こみっと

---
+++ OculusGame/Assets/MyFolders/SatoshiIshida/EnemyData.cs
@@ -0,0 +1,22 @@
+﻿// 2/17:Damage関数に処理追加 by石田
+using UnityEngine;
+using System.Collections;
+
+public class EnemyData : MonoBehaviour
+{
+    public int enemyHP { get; private set; }             //EnemyのHP
+
+    //体力初期化
+    public void InitHP(int hp)
+    {
+        enemyHP = 100;
+    }
+
+    //ダメージ処理
+    public void Damage(int gensyo)
+    {
+        enemyHP -= gensyo;
+        //ResourcesManager使用可能次第コメント解除してください
+        //EffectFactory.Instance.Create("hit", transform.position, transform.rotation);
+    }
+}
