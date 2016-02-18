d740ce26f61817195e87283a65f80287630a1a9d
SuzukiRyohei <ryo0718s@yahoo.co.jp>
2016-02-18 15:22:26 +0900

    Player2Dの弾発射

--- OculusGame/Assets/MyFolders/DaikiUmemura/player2D/Player2D.cs
+++ OculusGame/Assets/MyFolders/DaikiUmemura/player2D/Player2D.cs
@@ -3,7 +3,15 @@
 // 2/14:Turn処理追加、調整 by梅村
 // 2/15:Circumference追加 by鈴木
 // 2/16:Circumference更新 by鈴木
+/////////////////////////////
+// 2016/02/17 中村圭吾
+// bulletを呼び出す部分の変更
+// 弾のgameobjectの追加
+// ただしresource追加の許可を得るまでの措置
+/////////////////////////////
 // 2016/02/17 梅村 ui関連(lifes)の紐づけ
+// 2016/02/18 鈴木
+// bullet初期位置バグ修正
 using UnityEngine;
 using System.Collections;
 
@@ -11,45 +19,40 @@ public class Player2D : MonoBehaviour {
 	int playerTurnDirection;//1の時右向き,2のとき左向き
 	int playerQuater;
 	[SerializeField]
-	PlayerData2D playerData;
-	int shotInterval;	//弾の感覚管理
+	PlayerData2D playerData2D;
 	Vector3 Pos2D;	//復活用position保存
 	[SerializeField]
 	GameObject playerObject;
-
+	[SerializeField]
+	GameObject bullet;		//resourceに追加するのは許可を得てからやるために
 	[SerializeField]
 	AllUI allUI;
 	void Awake () {
 		allUI = GameObject.Find("UICanvas").GetComponent<AllUI>();
 		playerTurnDirection = 1;
 		playerQuater = 0;
-		playerData.speed = 1.0f;
-		playerData.pi = 3.14f;
-		playerData.resurrectionTime = 1.0f;
+		playerData2D.speed = 0.5f;
+		playerData2D.pi = 3.14f;
+		playerData2D.resurrectionTime = 1.0f;
 	}
-
-	void Update(){
-		Move ();
-	}
-
+	
 	/***移動処理***/
 	public void Move(){
-		if (playerData.playerHP > 0) {
-			playerData.vectorZ = Input.GetAxisRaw ("HorizontalP1");
-			playerData.vectorY = Input.GetAxisRaw ("VerticalP1");
+		if (playerData2D.playerHP > 0) {
+			playerData2D.vectorZ = Input.GetAxisRaw ("HorizontalP2");
+			playerData2D.vectorY = Input.GetAxisRaw ("VerticalP2");
 		}
-		if (playerData.playerHP <= 0){
-			Debug.Log("2D_DEAD");
+		if (playerData2D.playerHP <= 0){
 			StartCoroutine("Resurrection");
 		}
 
-		if(Input.GetAxisRaw ("HorizontalP1") > 0 || Input.GetKey(KeyCode.D)){
+		if(Input.GetAxisRaw ("HorizontalP2") > 0 || Input.GetKey(KeyCode.D)){
 			if(playerTurnDirection == 2){
 				playerTurnDirection = 1;
 				StartCoroutine("RightTurn");
 			}
 		}
-		else if(Input.GetAxisRaw ("HorizontalP1") < 0 || Input.GetKey(KeyCode.A)){
+		else if(Input.GetAxisRaw ("HorizontalP2") < 0 || Input.GetKey(KeyCode.A)){
 			if(playerTurnDirection == 1){
 				playerTurnDirection = 2;
 				StartCoroutine("LeftTurn");
@@ -62,39 +65,41 @@ public class Player2D : MonoBehaviour {
 	
 	/***撃つ処理***/
 	public void BulletShot(){
-		if (Input.GetButton ("R1P1")) {
-			shotInterval++;
-			if (shotInterval / 2 != 0 || shotInterval / 5 != 0) {
-				Instantiate(ResourcesManager.Instance.GetResourceScene("Bullet2D"), transform.position, new Quaternion(0,0,0,0));
-			}
+		playerData2D.shotWaitTime += Time.deltaTime;
+		if (Input.GetButton ("R1P2") && playerData2D.shotWaitTime > playerData2D.intervalTime) {
+
+			//ResourcesManagerの処理に置き換えます
+			GameObject shotBullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+			shotBullet.GetComponent<Player2DBullet>().bulletInit('R',playerData2D.degree,transform.position.y);
+			playerData2D.shotWaitTime = 0;
+		}
+		else if (Input.GetButton ("L1P2") && playerData2D.shotWaitTime > playerData2D.intervalTime) {
+
+			//ResourcesManagerにー
+			GameObject shotBullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+			shotBullet.GetComponent<Player2DBullet>().bulletInit('L',playerData2D.degree,transform.position.y);
+			playerData2D.shotWaitTime = 0;
 		}
 
-		else if (Input.GetButton ("L1P1")) {
-			shotInterval++;
-			if (shotInterval / 2 != 0 || shotInterval / 5 != 0) {
-				Instantiate(ResourcesManager.Instance.GetResourceScene("Bullet2D"), transform.position,new Quaternion(0,1,0,0));
-			}
-		}
-
-		if (Input.GetButtonUp ("R1P1") || Input.GetButtonUp ("L1P1")) {
-			shotInterval = 0;
+		if (playerData2D.shotWaitTime > playerData2D.intervalTime) {
+			playerData2D.shotWaitTime = playerData2D.intervalTime;
 		}
 	}
 	
 	/***呼び出せばPlayer2Dの破壊され復活までの処理が実行される***/
 	public IEnumerator Resurrection(){
 		Pos2D = gameObject.transform.position;
-		playerData.vectorZ = -100000;
+		playerData2D.vectorZ = -100000;
 
 		allUI.UiUpdate ("Lifes2D",0);
 		allUI.UiUpdate ("ComboReset",0);
 
-		while (playerData.resurrectionTime > 0) {
-			playerData.resurrectionTime -= 1 * Time.deltaTime;
-			playerData.InitHP();
+		while (playerData2D.resurrectionTime > 0) {
+			playerData2D.resurrectionTime -= 1 * Time.deltaTime;
+			playerData2D.InitHP();
 			yield return null;
 		}
-		playerData.resurrectionTime = 1.0f;
+		playerData2D.resurrectionTime = 1.0f;
 		gameObject.transform.position = Pos2D;
 	}
 
@@ -121,18 +126,18 @@ public class Player2D : MonoBehaviour {
 	}
 
 	/***呼び出せば回る 回る大きさはInspectorのRadiusを調整で変更***/
-	public void Circumference(){
+	void Circumference(){
 		
-		if(playerData.vectorZ > 0){
-			playerData.degree++;
+		if(playerData2D.vectorZ > 0){
+			playerData2D.degree++;
 		}
-		else if(playerData.vectorZ < 0){
-			playerData.degree--;
+		else if(playerData2D.vectorZ < 0){
+			playerData2D.degree--;
 		}
-		playerData.position.x = playerData.radius * Mathf.Cos (playerData.pi / 180 * playerData.degree);
-		playerData.position.y += playerData.vectorY * playerData.speed;
-		playerData.position.z = playerData.radius * Mathf.Sin (playerData.pi / 180 * playerData.degree);
-		transform.position = playerData.position;
-		transform.eulerAngles = new Vector3 (0, -playerData.degree,0);
+		playerData2D.position.x = playerData2D.radius * Mathf.Cos (playerData2D.pi / 180 * playerData2D.degree);
+		playerData2D.position.y += playerData2D.vectorY * playerData2D.speed;
+		playerData2D.position.z = playerData2D.radius * Mathf.Sin (playerData2D.pi / 180 * playerData2D.degree);
+		transform.position = playerData2D.position;
+		transform.eulerAngles = new Vector3 (0, -playerData2D.degree,0);
 	}
 }
---
+++ OculusGame/Assets/MyFolders/RyoheiSuzuki/player2d/Player2DBullet.cs
@@ -0,0 +1,80 @@
+﻿//////////////////////////////
+// 
+// 2016/02/16
+// 中村圭吾
+// player2dの弾作成
+// 円周移動
+// 初期位置のバグ
+// Awake時に角度が0なのが問題
+//////////////////////////////
+// 2016/02/18 鈴木
+// 初期位置のバグ修正
+// BulletDestroy修正
+// class内容変更
+
+using UnityEngine;
+using System.Collections;
+
+public class Player2DBullet : MonoBehaviour {
+
+	public static readonly float PAI = 3.14f;
+	float bulletDegree; // 角度
+	[SerializeField]
+	float radius; // 半径
+	[SerializeField]
+	float bulletSpeed;
+	[SerializeField]
+	float degreeSpace;	// 発射する弾の位置調整用
+	float bulletLife;
+	Vector3 shotPosition;
+	int shotBulletWay;	// 発射する弾の向き
+	Vector3 bulletPosition;
+	//自身の進む方向と出現角度
+	//bulletInit(進む方向'R'or'L',出現角度　player2dの角度など);
+	public void bulletInit(char receiveVariable,float player2dDegree,float player2dPositionY){
+		bulletPosition.y = player2dPositionY;
+		if(receiveVariable == 'R'){
+			shotBulletWay = 1;
+			bulletDegree = player2dDegree+degreeSpace;
+		}else if(receiveVariable == 'L'){
+			shotBulletWay = -1;
+			bulletDegree = player2dDegree-degreeSpace;
+		}
+	}
+
+	// Use this for initialization
+	void Awake () {
+		shotPosition = transform.position;
+		bulletLife = radius * 1.4f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ShotMove ();
+	}
+	void degreeWay(int bulletWay){
+		switch (bulletWay) {
+		case 1:
+			bulletDegree += bulletSpeed;
+			break;
+		case -1:
+			bulletDegree -= bulletSpeed;
+			break;
+		}
+	}
+
+	void ShotMove(){
+		degreeWay (shotBulletWay);
+		bulletPosition.x = radius * Mathf.Cos (PAI / 180 * bulletDegree);
+		bulletPosition.z = radius * Mathf.Sin (PAI / 180 * bulletDegree);
+		transform.position = bulletPosition;
+		transform.eulerAngles = new Vector3 (0, -bulletDegree, 0);
+		BulletDestroy ();
+	}
+
+	void BulletDestroy(){
+		if(bulletLife < Vector3.Distance(transform.position, shotPosition)){
+			Destroy(gameObject);
+		}
+	}
+}
