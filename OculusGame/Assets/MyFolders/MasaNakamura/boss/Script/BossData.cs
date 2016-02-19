fcb115295d8299acdc101e49220694237bfed771
umemuradaiki <i_like_uverworld__daiki__716@docomo.ne.jp>
2016-02-20 00:16:12 +0900

    Boss 梅村怒りのリファクタ

---
+++ OculusGame/Assets/MyFolders/MasaNakamura/boss/Script/BossData.cs
@@ -0,0 +1,55 @@
+﻿// 2/19 梅村 ボスのデータ
+using UnityEngine;
+using System.Collections;
+
+public class BossData : MonoBehaviour {
+	public const int target2D = 1;
+	public const int target3D = 2;
+
+	public int bossHp { private set; get;}
+	public int bossHate { set; get;}//+10で2Dを狙う -10で3Dを狙う
+	public int bossAttackTarget { set; get;}
+
+	void Awake(){
+		bossHp = 400;
+		bossAttackTarget = target2D;
+	}
+	/// <summary>
+	/// ダメージ処理
+	/// </summary>
+	/// <param name="bullet">弾の種類</param>
+	/// <param name="player">攻撃しているプレイヤー</param>
+	public void BossDamage(string bullet,string player){
+		Debug.Log (player + "ダメージ");
+
+		switch(bullet){
+		case "NomalBullet":
+			bossHp -= 2;
+			break;
+		default:
+			Debug.LogError("bullet指定ミス");
+			break;
+		}
+
+		switch(player){
+		case "2D":
+			bossHate++;
+			break;
+		case "3D":
+			bossHate--;
+			break;
+		default:
+			Debug.LogError("player指定ミス");
+			break;
+		}
+
+		if(bossHate >= 10){
+			bossAttackTarget = target2D;
+			bossHate = 0;
+		}
+		if(bossHate <= -10){
+			bossAttackTarget = target3D;
+			bossHate = 0;
+		}
+	}
+}
---
+++ OculusGame/Assets/MyFolders/MasaNakamura/boss/Script/Railgun.cs
@@ -0,0 +1,25 @@
+﻿// 2/19 まさ 他のボス関係との結合
+// 2月～19日　瀧本　基礎(TargetBullet,OnCollisionEnter);
+
+using UnityEngine;
+using System.Collections;
+
+public class Railgun : MonoBehaviour {
+	void Awake(){
+		Destroy (gameObject,3f);
+	}
+	//方向変換
+	//狙うプレイヤーのVector3	入れて呼ぶ
+	public void TargetBullet(Vector3 target){
+		transform.LookAt (target);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if(collision.gameObject.tag=="Player3D" ||collision.gameObject.tag=="Player2D") 
+		{
+			//ダメージ判定
+		}
+	}
+
+
+}
