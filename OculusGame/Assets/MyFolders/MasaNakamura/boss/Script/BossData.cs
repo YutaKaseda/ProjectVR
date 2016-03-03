916b13c83dec6cdf34e9f41dc7e53ccc5ce1be7b
Yutakaseda <yutakaseda3216@gmail.com>
2016-03-03 16:41:06 +0900

    いろいろなおしたゾイ

--- OculusGame/Assets/MyFolders/MasaNakamura/boss/Script/BossData.cs
+++ OculusGame/Assets/MyFolders/MasaNakamura/boss/Script/BossData.cs
@@ -87,7 +87,7 @@ public class BossData : MonoBehaviour {
         {
             EffectFactory.Instance.Create("dust", transform.position,transform.rotation, 0.5f);
            
-            yield return new WaitForSeconds(1.0f);
+            yield return new WaitForSeconds(3.0f);
         }
     }
 
