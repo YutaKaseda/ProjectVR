c22ea398d43ec3fdc55cacd77344eed8cd47e6fe
umemuradaiki <i_like_uverworld__daiki__716@docomo.ne.jp>
2016-03-05 09:19:46 +0900

    ホーミング弾

--- OculusGame/Assets/MyFolders/RyoheiSuzuki/player2d/PlayerData2D.cs
+++ OculusGame/Assets/MyFolders/RyoheiSuzuki/player2d/PlayerData2D.cs
@@ -2,14 +2,16 @@
 // 2/16:class内容追加 by瀧本
 // 2016/02/18 鈴木
 // class内容変更
+// 2016/03/04 梅村　ミサイル関連追加
 using UnityEngine;
 using System.Collections;
 
 public class PlayerData2D : PlayerData {
-
 	[SerializeField]
 	public float intervalTime;	//弾を撃てるようになるまでの時間
 	public float shotWaitTime { get; set;}	//現在待機してる時間
+	public float missileIntervalTime;	//missileを撃てるようになるまでの時間
+	public float missileWaitTime { get; set;}	//現在待機してる時間
 	public float resurrectionTime{ get; set; }		//復活時のクールタイム
 	public float degree{ get; set;}
 	[SerializeField]
