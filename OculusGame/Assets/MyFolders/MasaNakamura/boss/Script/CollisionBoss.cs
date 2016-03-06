02a858ede6e678f0c0ca318c9cb144775c819702
umemuradaiki <i_like_uverworld__daiki__716@docomo.ne.jp>
2016-03-06 09:05:39 +0900

    必殺技

--- OculusGame/Assets/MyFolders/MasaNakamura/boss/Script/CollisionBoss.cs
+++ OculusGame/Assets/MyFolders/MasaNakamura/boss/Script/CollisionBoss.cs
@@ -1,23 +1,27 @@
 ﻿// 2/19 梅村 ボスのあたり判定
+// 2016/03/6 梅村 必殺技のあたり判定
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 
 public class CollisionBoss : MonoBehaviour {
-
+	
 	BossData bossData;
-
-    void Awake()
-    {
+	
+	void Awake()
+	{
 		bossData = GetComponent<BossData>();
-    }
-
+	}
+	
 	void OnTriggerEnter(Collider other){
 		switch (other.gameObject.tag) {
 			
 		case "Player2DBullet":
 			bossData.BossDamage("NormalBullet","2D");
+			break;
+		case "SpecialArts":
+			bossData.BossDamage("SpecialArts","2D");
 			break;
 		}
 	}
--- OculusGame/Assets/MyFolders/SigeruTakimoto/ui/AllUI.cs
+++ OculusGame/Assets/MyFolders/SigeruTakimoto/ui/AllUI.cs
@@ -1,15 +1,16 @@
 ﻿//2016/02/18 梅村 UIの大本を作成。更新したいUIの名前と、値があれば正しい値を入れることでアクセスしてくれる
 // 2016/02/19 梅村 全体的に見やすくなったかな？
+// 2016/03/6 梅村 UseSpecialArts(必殺技)追加
 using UnityEngine;
 using System.Collections;
 
 public class AllUI : MonoBehaviour {
-
+	
 	KillCombo killCombo;
 	Score score;
 	DeathBlowGauge deathBlowGauge;
 	Lifes2D lifes2D;
-
+	
 	// Use this for initialization
 	void Awake () {
 		killCombo = GetComponent<KillCombo>();
@@ -32,6 +33,9 @@ public class AllUI : MonoBehaviour {
 		case "DeathBlowGaugeUp":
 			deathBlowGauge.GaugeUp();
 			break;
+		case "UseSpecialArts":
+			deathBlowGauge.UseLaser();
+			break;
 		case "Lifes2D":
 			lifes2D.ReduceLife ();
 			break;
@@ -40,6 +44,6 @@ public class AllUI : MonoBehaviour {
 			break;
 		}
 	}
-
-
+	
+	
 }
